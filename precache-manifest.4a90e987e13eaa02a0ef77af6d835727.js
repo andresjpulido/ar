572d7cd5cdaea1a411455d880cbd416aa804a548
Andres <andresjpulido@gmail.com>
2021-10-21 11:48:28 +1300

    Updates

---
+++ precache-manifest.4a90e987e13eaa02a0ef77af6d835727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8257921c5b2ccacc7299c751e8b22b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2a5448cb949a602c32",
+    "url": "/static/css/main.ce9a3cd1.chunk.css"
+  },
+  {
+    "revision": "93a1ace53980a170dfcc",
+    "url": "/static/js/2.555f7a8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.555f7a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2a5448cb949a602c32",
+    "url": "/static/js/main.bbcfa9b6.chunk.js"
+  },
+  {
+    "revision": "0a6356d15107587c87c4",
+    "url": "/static/js/runtime-main.ebf281ab.js"
+  }
+]);
